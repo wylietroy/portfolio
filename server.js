6b3cce76939c3b91185f53e10a1f0b7b115394b9
wylietroy <wyliesebtroy@gmail.com>
2023-06-09 17:57:16 -0400

    yup

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+// const port = 3000;
+const { JSDOM } = require("jsdom");
+
+app.use(express.static(__dirname));
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+global.document = dom.window.document;
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all the navigation links
+  var navLinks = document.querySelectorAll("nav ul li a");
+
+  // Attach click event listeners to each link
+  navLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+
+      // Get the target section from the href attribute
+      var targetSection = document.querySelector(link.getAttribute("href"));
+
+      // Scroll to the target section smoothly
+      targetSection.scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+});
+  
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
